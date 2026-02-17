c7e5a5154716e480ac911af242ba8ae07c63f268
Meg7373 <meaghannlw@gmail.com>
2026-02-16 20:31:31 -0500

    update all

--- client/Components/School/ListStudents.jsx
+++ client/Components/School/ListStudents.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function ListStudents() {
   return (
+    <Table striped>
+        <thead>
+            <tr>
+                <th>First Name </th>
+                <th>Last Name</th>
+                <th>Email</th>
+            </tr>
+        </thead>
+    </Table>
     
   )
 }
 
+useEffect(() => {
+    axios.get ("http")
+})
 export default ListStudents
